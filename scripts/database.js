e5f854e8ccb2cb6cfd833343dcc2d98626385996
Tim Gabets <tim@gabets.ru>
2017-10-12 21:01:54 +0300

    Added dummy database class

---
+++ scripts/database.js
@@ -0,0 +1,19 @@
+
+class Database {
+   constructor() {
+      console.log('Constructed');
+   }
+
+/*   
+   get area() {
+      return this.calcArea();
+   }
+
+   calcArea() {
+      return this.height * this.width;
+   }
+*/
+}
+
+const db = new Database();
+
